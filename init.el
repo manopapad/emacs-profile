ac1ab84354d073736be6749bce6b5f784f809d2d
Manolis Papadakis <mpapadakis@nvidia.com>
2020-04-24 12:12:07 -0700

    Lighter color for long lines background

--- init.el
+++ init.el
@@ -151,7 +151,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(whitespace-line ((t (:background "LightSalmon")))))
+ '(whitespace-line ((t (:background "misty rose")))))
 
 ;; Enable sensible word wrapping by default
 (global-visual-line-mode 1)
